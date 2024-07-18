dc4f7a28d4bc2b0c4718c8a812d6ac9c13a1cc9f
KrishnaJha <jhak6587@gmail.com>
2024-07-18 13:06:19 +0530

    final stage

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,4 +1,5 @@
 import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
@@ -9,6 +10,7 @@ const Conversations = () => {
 				<Conversation
 					key={conversation._id}
 					conversation={conversation}
+					emoji={getRandomEmoji()}
 					lastIdx={idx === conversations.length - 1}
 				/>
 			))}
@@ -18,3 +20,20 @@ const Conversations = () => {
 	);
 };
 export default Conversations;
+
+// STARTER CODE SNIPPET
+// import Conversation from "./Conversation";
+
+// const Conversations = () => {
+// 	return (
+// 		<div className='py-2 flex flex-col overflow-auto'>
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 			<Conversation />
+// 		</div>
+// 	);
+// };
+// export default Conversations;
